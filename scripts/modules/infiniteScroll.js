3d63ebfea42476a0d9a1e07c416bebae76e74fb3
Guillaume <gui.allegret@gmail.com>
2016-02-01 20:36:43 +0100

    Implement Infinite Scroll

--- scripts/modules/infiniteScroll.js
+++ scripts/modules/infiniteScroll.js
@@ -1,1 +1,102 @@
-(function(){    function factory(Utils, TickTimeout){        var attach = Utils.Event.attach;        var detach = Utils.Event.remove;        function InfiniteScroll(container){            this.__scrollHandle = TickTimeout(this.__scrollHandle, 50, this);            this.container = container;            attach(container, 'scroll', this.__scrollHandle);        }        Utils.prototize(InfiniteScroll, /** @lends {InfiniteScroll.prototype} */{            /**             *             * @param ev             * @private             */            __scrollHandle : function(){                console.log(window.scrollX);                console.log(window.scrollY);                /*var scroll, scrollMax;                if (this.settings.shouldCheck && this.settings.direction === 'horizontal') {                    scroll = body.clientHeight - window.innerHeight - wScrollY();                    scrollMax = window.innerHeight > this.settings.distance ? window.innerHeight : this.settings.distance;                    if (scroll < scrollMax) {                        this.settings.callback();                    }                } else if (this.settings.shouldCheck && this.settings.direction === 'vertical') {                    scroll = body.clientWidth - window.innerWidth - wScrollX();                    scrollMax = window.innerWidth > this.settings.distance ? window.innerWidth : this.settings.distance;                    if (scroll < scrollMax) {                        this.settings.callback();                    }                }*/            },            /**             *             */            destroy : function(){                detach(this.container, 'scroll', this.__scrollHandle);                this.container = null;            }        });        return InfiniteScroll;    }    Ark.define('InfiniteScroll', factory, ['Utils', 'TickTimeout']);})();
+(function(){
+
+    function factory(Utils, Dom, TickTimeout){
+        var attach = Utils.Event.attach;
+        var detach = Utils.Event.remove;
+        var winSize = Dom.Win.getSize;
+        var winScroll = Dom.Win.getScroll;
+
+        /**
+         * @constructor
+         *
+         * @param {Element} container
+         * @param {Object} [settings]
+         */
+        function InfiniteScroll(container, settings){
+            this.__scrollHandle = TickTimeout(this.__scrollHandle, 50, this);
+
+            this.eventie = Ark('Eventie');
+            this.container = container;
+            this.isEnable = false;
+            this.settings = Utils.merge({
+                direction : InfiniteScroll.DIRECTION_HORIZONTAL,
+                distance : 1000
+            }, settings);
+            this.enable();
+        }
+        
+        InfiniteScroll.DIRECTION_HORIZONTAL = 'horizontal';
+        InfiniteScroll.DIRECTION_VERTICAL = 'vertical';
+        
+        Utils.prototize(InfiniteScroll, /** @lends {InfiniteScroll.prototype} */{
+            /**
+             * @private
+             */
+            __scrollHandle : function(){
+                var scroll, scrollMax;
+                if (this.isEnable) {
+                    if(this.settings.direction === InfiniteScroll.DIRECTION_VERTICAL){
+                        var winWidth = winSize().width;
+                        scroll = Dom.getBody().clientWidth - winWidth - winScroll().x;
+                        scrollMax = winWidth > this.settings.distance ? winWidth : this.settings.distance;
+                    } else if(this.settings.direction === InfiniteScroll.DIRECTION_HORIZONTAL){
+                        var winHeight = winSize().height;
+                        scroll = Dom.getBody().clientHeight - winHeight - winScroll().y;
+                        scrollMax = winHeight > this.settings.distance ? winHeight : this.settings.distance;
+                    }
+                    if (scroll < scrollMax) {
+                        this.eventie.emit('scrolled');
+                    }
+                }
+            },
+            /**
+             * @param {Function} handler
+             */
+            on : function(handler){
+                this.eventie.on('scrolled', handler);
+            },
+            /**
+             * @param {Function} handler
+             */
+            off : function(handler){
+                this.eventie.off('scrolled', handler);
+            },
+            /**
+             * @return {InfiniteScroll}
+             */
+            enable : function() {
+                if(!this.isEnable){
+                    this.isEnable = true;
+                    attach(this.container, 'scroll', this.__scrollHandle);
+                }
+                return this;
+            },
+            
+            /**
+             * @return {InfiniteScroll}
+             */
+            disable : function() {
+                if(this.isEnable){
+                    this.isEnable = false;
+                    detach(this.container, 'scroll', this.__scrollHandle);
+                }
+                return this;
+            },
+            
+            /**
+             *
+             */
+            destroy : function(){
+                this.disable();
+                this.eventie.destroy();
+                this.eventie = null;
+                this.isEnable = null;
+                this.container = null;
+            }
+        });
+
+        return InfiniteScroll;
+    }
+
+    Ark.define('InfiniteScroll', factory, ['Utils', 'Dom', 'TickTimeout']);
+})();
